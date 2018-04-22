e7b12aa9b8bc0a0a1779edc220bbe767991ed44c
Sadaf-Faraz <sadaffaraz17@gmail.com>
2018-04-22 01:17:25 -0400

    Coming Soon
    Coming Soon TEMP design

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded',function(event){
+    var getText = [ "Designed by Sadaf....COMING SOON!"];
+
+   
+    
+    function typeWriter(text, x) {
+      // check if text isn't finished yet
+      if (x < (text.length)) {
+        // add next character to h1
+       document.querySelector('h1').innerHTML = text.substring(0, x+1) +'<span> </span>';
+        setTimeout(function() {
+          typeWriter(text, x + 1)
+        }, 100);
+    }
+}
+    
+     function StartAnimation(x) {
+       if (typeof getText[x] == 'undefined'){
+          setTimeout(function() {
+            StartAnimation(0);
+          });
+       }
+       if (x <  getText[x].length) {
+        // text exists.... start typewriter animation
+       typeWriter(getText[x], 0, function(){
+         StartAnimation(x + 1);
+       });
+      }
+    }
+
+    StartAnimation(0);
+  });
