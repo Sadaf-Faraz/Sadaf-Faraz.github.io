01ff98f1eeb8c250c4c485c284c2c6bd2e8a5b98
Sadaf-Faraz <sadaffaraz17@gmail.com>
2018-04-23 17:06:37 -0400

    Sample of Redesign

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 document.addEventListener('DOMContentLoaded',function(event){
-    var getText = [ "Designed By Sadaf....will launch very soon!"];
+  var getText = [ "Designed by Sadaf"];
 
-   
-    
-    function typeWriter(text, x) {
-      // check if text isn't finished yet
-      if (x < (text.length)) {
-        // add next character to h1
-       document.querySelector('h1').innerHTML = text.substring(0, x+1) +'<span> </span>';
-        setTimeout(function() {
-          typeWriter(text, x + 1)
-        }, 100);
-    }
+ 
+  
+  function typeWriter(text, x) {
+    // check if text isn't finished yet
+    if (x < (text.length)) {
+      // add next character to h1
+     document.querySelector('.headertext').innerHTML = text.substring(0, x+1) +'<span> </span>';
+
+      setTimeout(function() {
+        typeWriter(text, x + 1)
+      }, 80);
+  }
 }
-    
-     function StartAnimation(x) {
-       if (typeof getText[x] == 'undefined'){
-          setTimeout(function() {
-            StartAnimation(0);
-          });
-       }
-       if (x <  getText[x].length) {
-        // text exists.... start typewriter animation
-       typeWriter(getText[x], 0, function(){
-         StartAnimation(x + 1);
-       });
-      }
+  
+   function StartAnimation(x) {
+     if (typeof getText[x] == 'undefined'){
+        setTimeout(function() {
+          StartAnimation(0);
+        });
+     }
+     if (x <  getText[x].length) {
+      // text exists.... start typewriter animation
+     typeWriter(getText[x], 0, function(){
+       StartAnimation(x + 1);
+     });
     }
+  }
 
-    StartAnimation(0);
-  });
+  StartAnimation(0);
+});
+
